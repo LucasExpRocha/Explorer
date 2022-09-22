64bec7fb65362d4df78fa5473d8b836d8e6ae511
Lucas Exposito Rocha <107315686+LucasExpRocha@users.noreply.github.com>
2022-09-21 23:20:48 -0300

    mudança de rota spa, continuação stage 08

--- Stage08/Api/src/routes/notes.routes.js
+++ Stage08/Api/src/routes/notes.routes.js
@@ -6,6 +6,10 @@ const notesRoutes = Router()
 
 const notesController = new NotesController();
 
+notesRoutes.get('/', notesController.listNotes);
 notesRoutes.post('/:user_id', notesController.createNotes);
+notesRoutes.get('/:id', notesController.show);
+notesRoutes.delete('/:id', notesController.delete)
+
 
 module.exports = notesRoutes;
