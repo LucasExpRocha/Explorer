80baf5a3c9be76db73f22a2a5e95ed19c321783a
Lucas Exposito Rocha <107315686+LucasExpRocha@users.noreply.github.com>
2022-09-29 17:48:44 -0300

    Init stage09 rocketnotes

--- Stage08/Api/src/routes/index.js
+++ Stage08/Api/src/routes/index.js
@@ -8,7 +8,6 @@ const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
-routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRoutes);
 
 
---
+++ Stage09/Rocketnotes/src/pages/details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+export function Details() {
+
+  return (
+    <Container>
+      <h1>Hello</h1>
+      <span>my brother</span>
+    </Container>
+  )
+}
