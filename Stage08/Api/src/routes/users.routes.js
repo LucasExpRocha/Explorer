023b2d7f2d636062a72a213fa3f4b8bfaa2e4784
Lucas Exposito Rocha <107315686+LucasExpRocha@users.noreply.github.com>
2022-10-26 23:27:17 -0300

    upload image, install cors

--- Stage08/Api/src/routes/users.routes.js
+++ Stage08/Api/src/routes/users.routes.js
@@ -2,19 +2,18 @@ const { Router, response } = require('express');
 const multer = require('multer');
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 const uploadConfig = require('../configs/upload')
 
 const userRoutes = Router()
-const upload = multer(uploadConfig.multer);
+const upload = multer(uploadConfig.MULTER);
 
 const userController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 userRoutes.post('/', userController.createUser);
 userRoutes.put('/', ensureAuthenticated, userController.update);
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-    console.log(req.file.filename);
-    res.json()
-})
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = userRoutes;
--- Stage08/Api/src/server.js
+++ Stage08/Api/src/server.js
@@ -1,15 +1,19 @@
 require("express-async-errors")
 
-const express = require('express'); //import
-const app = express(); //init express
+const express = require('express');
+const cors = require('cors');
 
 const migrationsRun = require("./database/sqlite/migrations")
 const routes = require('./routes');
 const AppError = require('./utils/AppError')
+const uploadConfig = require("./configs/upload")
+
+const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 app.use(routes)
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 migrationsRun();
 
