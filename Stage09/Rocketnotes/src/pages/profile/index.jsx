c60d808937a26ccdccbd97a190c52bfa3796e882
Lucas Exposito Rocha <107315686+LucasExpRocha@users.noreply.github.com>
2022-10-29 23:31:52 -0300

    Correcao header

--- Stage09/Rocketnotes/src/pages/profile/index.jsx
+++ Stage09/Rocketnotes/src/pages/profile/index.jsx
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom';
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth'
+
 import { Container, Form, Avatar } from "./styles";
 import { Input } from "../../components/input"
 import { Button } from "../../components/buttons";
+import { useState } from "react";
 
 
 export function Profile(){
+    const { user } = useAuth();
+
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [passwordOld, setPasswordOld] = useState();
+    const [passwordNew, setPasswordNew] = useState();
+
     return (
         <Container>
             <header>
@@ -22,10 +33,10 @@ export function Profile(){
                         <input type="file" id="avatar" accept='image/*'/>
                     </label>
                 </Avatar>
-                <Input placeholder="Nome" type="text" icon={FiUser}/>
-                <Input placeholder="E-mail" type="text" icon={FiMail}/>
-                <Input placeholder="Senha atual" type="password" icon={FiLock}/>
-                <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+                <Input placeholder="Nome" value={name} onChange={e => setName(e.target.value)} type="text" icon={FiUser}/>
+                <Input placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} type="text" icon={FiMail}/>
+                <Input placeholder="Senha atual" onChange={e => setPasswordOld(e.target.value)} type="password" icon={FiLock}/>
+                <Input placeholder="Nova senha" onChange={e => setPasswordNew(e.target.value)} type="password" icon={FiLock}/>
                 <Button title="Salvar" />
             </Form>
         </Container>
